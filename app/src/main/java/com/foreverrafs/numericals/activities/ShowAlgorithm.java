ff3b3095a3190bf16132ccb3750515b5db2dd8d1
Aziz Rafsanjani <coded_raf@yahoo.com>
2018-08-23 07:02:17 +0000

    include toolbar in show algorithm activity

--- app/src/main/java/com/foreverrafs/numericals/activities/ShowAlgorithm.java
+++ app/src/main/java/com/foreverrafs/numericals/activities/ShowAlgorithm.java
@@ -4,9 +4,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.foreverrafs.numericals.R;
 import com.foreverrafs.numericals.utils.Utilities;
@@ -20,6 +22,19 @@ public class ShowAlgorithm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_algorithm);
         Bundle bundle = getIntent().getExtras();
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(myToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        //set the typeface of the text which appears on the toolbar
+        TextView toolbarAppTitle = findViewById(R.id.toolbarAppTitle);
+        toolbarAppTitle.setText("Algorithm");
+        Utilities.setToolbarTypeface(this, toolbarAppTitle);
 
         if (Build.VERSION.SDK_INT >= 24) {
             try {
