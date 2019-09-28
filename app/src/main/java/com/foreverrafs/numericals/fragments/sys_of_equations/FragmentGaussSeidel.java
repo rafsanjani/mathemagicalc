13a08e933ea307de73a204f3d1fd93e7125bb7c9
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-09-28 17:33:04 +0000

    Fix NPE in Gauss Seidel Fragment

--- app/src/main/java/com/foreverrafs/numericals/fragments/sys_of_equations/FragmentGaussSeidel.java
+++ app/src/main/java/com/foreverrafs/numericals/fragments/sys_of_equations/FragmentGaussSeidel.java
@@ -6,14 +6,12 @@ import android.os.Handler;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,34 +23,41 @@ import com.foreverrafs.numericals.R;
 import com.foreverrafs.numericals.activities.ShowAlgoActivity;
 import com.foreverrafs.numericals.core.Numericals;
 import com.foreverrafs.numericals.utils.Utilities;
-import com.google.android.material.textfield.TextInputLayout;
 
 import org.apache.commons.math3.util.Precision;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
  * Created by Aziz Rafsanjani on 11/4/2017.
  */
 
-public class FragmentGaussSeidel extends Fragment implements View.OnClickListener, TextWatcher, View.OnKeyListener {
+public class FragmentGaussSeidel extends Fragment implements TextWatcher, View.OnKeyListener {
 
-    View rootView;
+    @BindView(R.id.btnCalculate)
+    Button btnCalculate;
+    @BindView(R.id.btnBack)
+    Button btnBack;
+    @BindView(R.id.parentContainer)
     ViewGroup viewGroup;
-    Handler handler = new Handler() {
+    private View rootView;
+    private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
-        public void handleMessage(Message msg) {
+        public boolean handleMessage(@NonNull Message msg) {
             Button btnCalculate = rootView.findViewById(R.id.btnCalculate);
-            btnCalculate.setText("CALCULATE");
+            btnCalculate.setText(getString(R.string.calculate));
 
             boolean success = msg.getData().getBoolean("success");
             if (!success) {
                 Toast.makeText(getContext(), msg.getData().getString("exception"), Toast.LENGTH_LONG).show();
 
-                return;
+                return false;
             }
             double[] solution = msg.getData().getDoubleArray("results");
             TextView tvAnswer = rootView.findViewById(R.id.tvAnswer);
-
 
             tvAnswer.setText("[ " +
                     Precision.round(solution[0], 2) + ", "
@@ -63,30 +68,25 @@ public class FragmentGaussSeidel extends Fragment implements View.OnClickListene
 
             //for transitions sake
             Utilities.animateAnswer(tvAnswer, viewGroup, Utilities.DisplayMode.SHOW);
-            Utilities.animateAnswer(tvAnswer, rootView.findViewById(R.id.parentContainer), Utilities.DisplayMode.SHOW);
 
+            return true;
         }
-    };
+    });
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_gaussseidel, container, false);
+
+        ButterKnife.bind(this, rootView);
         initControls();
+
         return rootView;
     }
 
+
     public void initControls() {
-        //Utilities.setTypeFace(rootView.findViewById(R.id.text_header), getContext(), Utilities.TypeFacename.raleway_bold);
-        Button btnCalculate = rootView.findViewById(R.id.btnCalculate);
-        Button btnBack = rootView.findViewById(R.id.btnBack);
-
-        TextInputLayout til = rootView.findViewById(R.id.omega_textInputLayout);
-
-        String omega = "\u03A9";
-        til.setHint(omega.toLowerCase());
-
-
         EditText[] etEqn = new EditText[3];
         etEqn[0] = rootView.findViewById(R.id.text_equationx1);
         etEqn[1] = rootView.findViewById(R.id.text_equationx2);
@@ -96,35 +96,23 @@ public class FragmentGaussSeidel extends Fragment implements View.OnClickListene
             editText.addTextChangedListener(this);
             editText.setOnKeyListener(this);
         }
-
-        btnCalculate.setOnClickListener(this);
-        btnBack.setOnClickListener(this);
-        rootView.findViewById(R.id.btnShowAlgo).setOnClickListener(this);
-
-
-        viewGroup = (LinearLayout) rootView.findViewById(R.id.parentContainer);
-        //("System of Equations", "Gauss Seidel's Method");
-
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnBack:
-                if (getActivity() != null) {
-                    getActivity().finish();
-                }
-                break;
 
-            case R.id.btnCalculate:
-                Log.i(Utilities.LOG_TAG, "performing jacobi's calculation");
-                onCalculate();
-                break;
-            case R.id.btnShowAlgo:
-                onShowAlgorithm();
-                break;
+    @OnClick(R.id.btnBack)
+    void onBackClicked() {
+        if (getActivity() != null)
+            getActivity().finish();
+    }
 
-        }
+    @OnClick(R.id.btnCalculate)
+    void onCalculateClicked() {
+        onCalculate();
+    }
+
+    @OnClick(R.id.btnShowAlgo)
+    void onShowAlgoClicked() {
+        onShowAlgorithm();
     }
 
     private void onShowAlgorithm() {
@@ -158,27 +146,23 @@ public class FragmentGaussSeidel extends Fragment implements View.OnClickListene
             }
 
             Button btnCalculate = rootView.findViewById(R.id.btnCalculate);
-            btnCalculate.setText("CALCULATING....");
+            btnCalculate.setText(getString(R.string.calculating));
 
 
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    double[] solution;
-                    Message message = new Message();
+            Runnable runnable = () -> {
+                double[] solution;
+                Message message = new Message();
 
-                    try {
-                        solution = Numericals.gaussSeidel(equations, initGuess, epsilon);
-                        message.getData().putDoubleArray("results", solution);
-                        message.getData().putBoolean("success", true);
-                    } catch (Exception ex) {
-                        message.getData().putBoolean("success", false);
-                        message.getData().putString("exception", ex.getMessage());
-                        System.out.println(ex.getMessage());
-                    }
-                    handler.sendMessage(message);
+                try {
+                    solution = Numericals.gaussSeidel(equations, initGuess, epsilon);
+                    message.getData().putDoubleArray("results", solution);
+                    message.getData().putBoolean("success", true);
+                } catch (Exception ex) {
+                    message.getData().putBoolean("success", false);
+                    message.getData().putString("exception", ex.getMessage());
+                    System.out.println(ex.getMessage());
                 }
-
+                mHandler.sendMessage(message);
             };
 
             Thread thread = new Thread(runnable);
