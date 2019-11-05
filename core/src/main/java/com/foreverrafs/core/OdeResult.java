fdd4f32b6585d328c4e951c9efeb8e890a7d1873
Aziz Rafsanjani <coded_raf@yahoo.com>
2019-11-05 22:38:02 +0000

    Make Ordinary Diff. Equations model class parcelable for passing via bundles

--- core/src/main/java/com/foreverrafs/core/OdeResult.java
+++ core/src/main/java/com/foreverrafs/core/OdeResult.java
@@ -1,6 +1,9 @@
 package com.foreverrafs.core;
 
-public class OdeResult {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class OdeResult implements Parcelable {
     private double x, y, n;
 
     public OdeResult(double y, double x, double n) {
@@ -9,6 +12,24 @@ public class OdeResult {
         this.n = n;
     }
 
+
+    public static final Creator<OdeResult> CREATOR = new Creator<OdeResult>() {
+        @Override
+        public OdeResult createFromParcel(Parcel in) {
+            return new OdeResult(in);
+        }
+
+        @Override
+        public OdeResult[] newArray(int size) {
+            return new OdeResult[size];
+        }
+    };
+
+    protected OdeResult(Parcel in) {
+        x = in.readDouble();
+        y = in.readDouble();
+        n = in.readDouble();
+    }
 
     public double getX() {
         return x;
@@ -30,4 +51,15 @@ public class OdeResult {
         this.y = y;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeDouble(x);
+        parcel.writeDouble(y);
+        parcel.writeDouble(n);
+    }
 }
